f110cc0f0cc6b837436c3626958b6ceee6f376eb
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-09-29 10:13:43 -0700

    edit images

--- components/home/Body.jsx
+++ components/home/Body.jsx
@@ -6,21 +6,21 @@ function Body(){
             <div className="group-btns">
                 <div className="button-head">
                     <p className="button-name"> مستلزمات التجميل</p>
-                    <img src="/beauty-tools-concept-3d-illustration-icon-composition-with-blackboard-with-curling-iron-vector_9209-9241.jpg" alt="" />
+                    <img src="./beauty-tools-concept-3d-illustration-icon-composition-with-blackboard-with-curling-iron-vector_9209-9241.jpg" alt="" />
                 </div>
                 <div className="button-head">
                     
                     <p className="button-name">الملابس الجاهزه</p>
-                    <img src="/old-wardrobe-clothes-icon-cartoon-style-vector-36842358.jpg" alt="" />
+                    <img src="./old-wardrobe-clothes-icon-cartoon-style-vector-36842358.jpg" alt="" />
                     
                 </div>
                 <div className="button-head">
                     <p className="button-name">الاحذية الراقية</p>
-                    <img src="/cute-shoe-icon-free-vector.jpg" alt="" />
+                    <img src="./cute-shoe-icon-free-vector.jpg" alt="" />
                 </div>
                 <div className="button-head">
                     <p className="button-name">الماركات العالمية</p>
-                    <img src="/logo-brand-clothing-on-a-white-background-vector-22770200.jpg" alt="" />
+                    <img src="./logo-brand-clothing-on-a-white-background-vector-22770200.jpg" alt="" />
                 </div>
             </div>
             <div className="card-groub">
--- components/home/header.jsx
+++ components/home/header.jsx
@@ -6,9 +6,9 @@ function Header(){
             <div className="continar">
             <div className="icons">
             
-            <img className="img-header" src="/menu.svg" alt="" />
-            <img className="img-header" src="/user.svg" alt="" />
-            <img className="img-header" src="/shopping_cart_FILL0_wght400_GRAD0_opsz48.svg" alt="" />
+            <img className="img-header" src="./menu.svg" alt="" />
+            <img className="img-header" src="./user.svg" alt="" />
+            <img className="img-header" src="./shopping_cart_FILL0_wght400_GRAD0_opsz48.svg" alt="" />
             </div>
             <h1 className="title-icon">Store Name</h1>
             </div>
